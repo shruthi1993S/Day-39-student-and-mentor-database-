2ae21d05559e8c711cf23ee5bb4ffac2d7656046
shruthi1993s <shruthiji06@gmail.com>
2024-06-21 22:30:49 +0530

    Mentor and Student Assigning with Database

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const routes = require('./route/routes');
-
+require ("dotenv").config()
 const app = express();
-const PORT = 3000;
+
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/mentor_student_db').then(()=>{
-    console.log("MongoDB connected successfully")
-  })
+mongoose.connect(process.env.MONGODB).then(()=>{
+  console.log("MongoDB connected successfully")
+})
 
 
 // Middleware
@@ -19,6 +19,8 @@ app.use(bodyParser.json());
 app.use('/', routes);
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+const PORT=process.env.PORT 
+ app.listen(PORT,()=>{
+    console.log(`Server running in the port ${PORT}`)
+ })
